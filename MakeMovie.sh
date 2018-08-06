4d310529538333b51eed6e8c03b44e40a69792f9
mutanthost <matthewhuston@gmail.com>
2018-08-06 10:47:58 -0600

    added finding and removing zero length files

--- MakeMovie.sh
+++ MakeMovie.sh
@@ -19,6 +19,8 @@ sleep 5
 echo "cleaning empty directories"
 find ./ -depth -type d -exec rmdir {} +
 sleep 2
+echo "find and clean empty files"
+find ./ -type f -size 0
 echo "creating list of descending files"
 ls -1v |grep jpg > files.txt
 # echo "trying out ffmpeg"
